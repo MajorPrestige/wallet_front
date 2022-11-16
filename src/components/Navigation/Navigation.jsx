fc1746b3bd7d85416793ff8fa424d7150c4aa3aa
Denys Dashkevych <denysdashkevych@gmail.com>
2022-11-16 19:44:53 +0200

    finished Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,55 @@
+import { useMediaQuery } from "react-responsive";
+
 import {
   StyledHomeIcon,
   StyledCurrencyIcon,
   StyledStatisticsIcon,
   ButtonWrapper,
   NavigateButton,
+  NavigateText,
 } from "./Navigation.styled";
 
 const Navigation = ({ current }) => {
+  const isTablet = useMediaQuery({ minWidth: 768 });
+
   return (
     <ButtonWrapper>
-      <NavigateButton type="button" to='/home'>
+      <NavigateButton type="button" to="/home">
         {current === "home" ? <StyledHomeIcon current="true" /> : <StyledHomeIcon />}
+        {isTablet ? (
+          current === "home" ? (
+            <NavigateText current="true">Home</NavigateText>
+          ) : (
+            <NavigateText>Home</NavigateText>
+          )
+        ) : null}
       </NavigateButton>
 
-      <NavigateButton type="button" to='/statistics'>
+      <NavigateButton type="button" to="/statistics">
         {current === "statistics" ? (
           <StyledStatisticsIcon current="true" />
         ) : (
           <StyledStatisticsIcon />
         )}
+
+        {isTablet ? (
+          current === "statistics" ? (
+            <NavigateText current="true">Statistics</NavigateText>
+          ) : (
+            <NavigateText>Statistics</NavigateText>
+          )
+        ) : null}
       </NavigateButton>
 
-      <NavigateButton type="button" to='/currency'>
-        {current === "currency" ? (
-          <StyledCurrencyIcon current="true" />
-        ) : (
-          <StyledCurrencyIcon />
-        )}
-      </NavigateButton>
+      {!isTablet && (
+        <NavigateButton type="button" to="/currency">
+          {current === "currency" ? (
+            <StyledCurrencyIcon current="true" />
+          ) : (
+            <StyledCurrencyIcon />
+          )}
+        </NavigateButton>
+      )}
     </ButtonWrapper>
   );
 };
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 import { NavLink } from "react-router-dom";
-// import { device } from "styles/Media.variables";
+import { device } from "styles/Media.variables";
 
 import { ReactComponent as Home } from "images/svgs/home.svg";
 import { ReactComponent as Statistics } from "images/svgs/statistics.svg";
@@ -10,6 +10,11 @@ export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 12px;
+
+  @media ${device.tablet} {
+    margin-top: 0px;
+    flex-direction: column;
+  }
 `;
 
 export const NavigateButton = styled(NavLink)`
@@ -17,27 +22,55 @@ export const NavigateButton = styled(NavLink)`
   background-color: transparent;
   border: none;
   height: 44px;
+  font-family: 'Poppins';
+  font-size: 18px;
+  color: #000000;
 
   &:not(:last-child) {
     margin-right: 32px;
   }
+
+  &:hover svg,
+  &:focus svg{
+    filter: drop-shadow(0px 3px 10px rgba(74, 86, 226, 0.5));
+    fill: #4a56e2;
+  }
+
+  @media ${device.tablet} {
+    display: flex;
+    align-items: center;
+    height: auto;
+
+    &:not(:last-child) {
+    margin-right: 0px;
+    margin-bottom: 12px;
+  }
+  }
+`;
+
+export const NavigateText = styled.span`
+  ${(props) =>
+    props.current &&
+    css`
+      font-weight: 700;
+    `}
 `;
 
 const styledIcon = css`
   fill: #6e78e8;
   transition: 0.2s ease-in-out;
 
-  &:hover,
-  &:focus {
-    filter: drop-shadow(0px 3px 10px rgba(74, 86, 226, 0.5));
-    fill: #4a56e2;
-  }
-
   ${(props) =>
     props.current &&
     css`
       fill: #4a56e2;
     `}
+
+    @media ${device.tablet} {
+      width: 24px;
+      height: 24px;
+      margin-right: 20px;
+    }
 `;
 
 export const StyledHomeIcon = styled(Home)`
