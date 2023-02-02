4a3fdeaca240c1561492c4ef89b08f4b82761020
NikolayLemehov <nicklemster@gmail.com>
2023-02-02 18:43:48 +0000

    Deploying to gh-pages from @ NikolayLemehov/wallet_front@522e1be6e4c2cb3d84ac3d122bff862163ea260c 🚀

--- static/js/449.8b7f2fe4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwallet=self.webpackChunkwallet||[]).push([[449],{2449:function(e,r,s){s.r(r);var u=s(7689),n=s(4805),t=s(4628),c=s(9167),l=s(5672),a=s(3387),i=s(6587),h=s(184);r.default=function(){return(0,n.useMediaQuery)({minWidth:768})?(0,h.jsx)(u.Fg,{to:"/home",replace:"true"}):(0,h.jsxs)(i.H_,{children:[(0,h.jsx)(c.Z,{}),(0,h.jsxs)(a.W2,{children:[(0,h.jsx)(l.Z,{current:"currency"}),(0,h.jsx)(t.Z,{})]})]})}}}]);
-//# sourceMappingURL=449.8b7f2fe4.chunk.js.map
---
+++ static/js/449.c0f2202f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet=self.webpackChunkwallet||[]).push([[449],{2449:function(e,r,s){s.r(r);var u=s(7689),n=s(4805),t=s(4628),c=s(1273),l=s(5672),a=s(3387),i=s(6587),h=s(184);r.default=function(){return(0,n.useMediaQuery)({minWidth:768})?(0,h.jsx)(u.Fg,{to:"/home",replace:"true"}):(0,h.jsxs)(i.H_,{children:[(0,h.jsx)(c.Z,{}),(0,h.jsxs)(a.W2,{children:[(0,h.jsx)(l.Z,{current:"currency"}),(0,h.jsx)(t.Z,{})]})]})}}}]);
+//# sourceMappingURL=449.c0f2202f.chunk.js.map
