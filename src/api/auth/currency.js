d7f6c8204da0fa4e35efab8132302060e5e3fcda
Anna Karachkovska <akarachkovska@gmail.com>
2023-02-03 19:19:47 +0200

    Update

--- src/api/auth/currency.js
+++ src/api/auth/currency.js
@@ -1,12 +1,3 @@
-// const getCurrencyCources = async () => {
-// 	try {
-// 		const { data } = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5");
-// 		return data;
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
 const getCurrencyCources = async () => {
 	try {
 		const { data } = await fetch("https://api.monobank.ua/bank/currency");
@@ -16,5 +7,3 @@ const getCurrencyCources = async () => {
 };
 getCurrencyCources();
 export default getCurrencyCources;
-
-// https://api.monobank.ua/bank/currency
