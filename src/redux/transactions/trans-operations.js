1b367cc22ed52ad87527aad8d0fb970ce3b7bca9
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-02-04 21:30:11 +0200

    fix: eslint errors

--- src/redux/transactions/trans-operations.js
+++ src/redux/transactions/trans-operations.js
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { axiosCurrent } from 'api/auth/auth';
+// import { axiosCurrent } from 'api/auth/auth';
 
 import { axiosStatistic } from 'api/transactions/statistic';
 
 import { transactionsAPI } from 'api/transactions/transactionsApi';
-import { getTransactions } from './trans-selectors.js';
+// import { getTransactions } from './trans-selectors.js';
 import { current } from '../auth/auth-operations.js';
 
 export const fetchStatistic = createAsyncThunk(
