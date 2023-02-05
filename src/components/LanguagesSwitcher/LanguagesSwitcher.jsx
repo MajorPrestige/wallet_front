dd9eb3a4b6169c59ee53ef55ec2bf43fa6df5e45
Leonid Holub <109361064+leonid-holub@users.noreply.github.com>
2023-02-06 00:32:20 +0200

    First part of fix
    1) Add logic for local storage
    2) The display of table fields has been fixed for mobile version

--- src/components/LanguagesSwitcher/LanguagesSwitcher.jsx
+++ src/components/LanguagesSwitcher/LanguagesSwitcher.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { GB, UA } from 'country-flag-icons/react/3x2';
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import {setLanguage} from 'redux/language/language-slice';
 
 export const LanguagesSwitcher = () => {
-const [setLng] = useState('');
+const [lng, setLng] = useState('');
 const dispatch = useDispatch();
 const { i18n } = useTranslation();
 
+const changeLanguage = useCallback((value) => {
+  i18n.changeLanguage(value);
+}, [i18n]);
+
+const saveInLocalStorage = (key, value) => {
+  try {
+    const prepearedValue = JSON.stringify(value);
+    localStorage.setItem(key, prepearedValue);
+  } catch (error) {
+    console.error("Set prepearedValue error: ", error.message);
+  }
+};
+
+const loadFromLocalStorage = key => {
+  try {
+    const parsedValue = localStorage.getItem(key);
+    return parsedValue === null ? undefined : JSON.parse(parsedValue);
+  } catch (error) {
+    console.error("Get parsedValue error: ", error.message);
+  }
+};
+
 const handleThemeChange = (lng) => {
-    const changeLanguage = (language) => {
-      i18n.changeLanguage(language);
-  };
       changeLanguage(lng);
       setLng(lng);
       dispatch(setLanguage(lng));
+      saveInLocalStorage("language", lng);
   };
+
+useEffect(() => {
+  const currentLanguage = loadFromLocalStorage("language");
+  if(currentLanguage){
+  setLng(currentLanguage);
+  changeLanguage(lng);
+  return;
+  };
+  changeLanguage('en');
+  setLng('en');
+  saveInLocalStorage("language", lng);
+},[changeLanguage, lng]);
 
 
 return (
@@ -27,7 +59,5 @@ return (
 <button type="button" style={{padding: "0",  border: "none", backgroundColor: "transparent", width: "50px", minHeight: "5px", borderRadius: "30%", overflow: "hidden"}} onClick={() => handleThemeChange('ua')}><UA title="UA" style={{ display: "block", width: "50px" }}/></button>
 </div>
 </>
-
 );
-
 };
