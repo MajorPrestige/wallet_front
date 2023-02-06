b08d4db360e9872cb0ff46304e6f10f2f865f91e
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-02-06 13:22:02 +0200

    fix: logo mobile

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,20 +1,22 @@
 import { LogoContainer, LogoHeaderContainer, LogoText } from './Logo.styled';
 import { IconLogo } from './Logo.styled';
 import { useTranslation } from "react-i18next";
+import { useMediaQuery } from 'react-responsive';
 
 const Logo = ({ header }) => {
+  const isNotSmallMobile = useMediaQuery({ minWidth: 480 });
   const { t } = useTranslation();
   return (
     <>
       {header ? (
         <LogoHeaderContainer to="/">
           <IconLogo />
-          <LogoText>{t('wallet')}</LogoText>
+          <LogoText>{isNotSmallMobile && t('wallet')}</LogoText>
         </LogoHeaderContainer>
       ) : (
         <LogoContainer>
          <IconLogo />
-          <LogoText>{t('wallet')}</LogoText>
+          <LogoText>{isNotSmallMobile && t('wallet')}</LogoText>
         </LogoContainer>
       )}
     </>
